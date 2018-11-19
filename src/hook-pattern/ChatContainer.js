a6621d211f88d14f75f756b37773d90ab2418383
Jaime Lopez <detec.azul@gmail.com>
2018-11-19 15:45:04 +0000

    Added hooks version of the Chatcontainer

--- src/hook-pattern/ChatContainer.js
+++ src/hook-pattern/ChatContainer.js
@@ -1,11 +1,45 @@
 import React from 'react';
+import { useState } from 'react';
 
 const HookChatContainer = () => {
-    return(
-        <div className="chat-container"> 
-            <p>Hooks Hello World</p>
-        </div>
-    )
-}
+  const [nameField, setNameField] = useState('');
+  const [messageField, setMessageField] = useState('');
+  const [chat, setChat] = useState([]);
+
+  const renderMessage = () => {
+    const message =
+      <p key={chat.length + 1}>
+        <strong>{nameField}: </strong>
+        {messageField}
+      </p>
+
+      setChat([...chat, message]);
+  };
+
+  return (
+    <div className="chat-container">
+      <div className="form">
+        <input
+          type="text"
+          name="name"
+          placeholder="Nickname"
+          onChange={event => {
+            setNameField(event.target.value);
+          }}
+        />
+        <input
+          type="text"
+          name="message"
+          placeholder="Type something witty"
+          onChange={event => {
+            setMessageField(event.target.value);
+          }}
+        />
+        <button onClick={renderMessage}>Send Message!</button>
+      </div>
+      <div className="chat-messages">{chat}</div>
+    </div>
+  );
+};
 
 export default HookChatContainer;
